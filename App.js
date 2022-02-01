de7d8faeab3c069d2444ab615ad8d163a5b9f268
aatituanav <alvaro.andres1996@hotmail.com>
2022-02-01 08:00:20 -0500

    implementacion de styled-components

--- App.js
+++ App.js
@@ -2,24 +2,24 @@
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
-import { theme } from "./src/infrastructure/theme"
+import { theme } from "./src/infrastructure/theme/index"
 
-import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald'
-import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
-
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 
 export default function App() {
 
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
-  })
+  });
+
   const [latoLoaded] = useLato({
     Lato_400Regular,
-  })
+  });
 
   if (!oswaldLoaded || !latoLoaded) {
-    return null
+    return null;
   }
 
   return (
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card, Paragraph } from "react-native-paper";
 import styled from "styled-components/native";
+import { theme } from "../../../infrastructure/theme/index"
+import { SvgXml } from 'react-native-svg';
+import { start } from '../../../../assets/star'
 
 const RestaurantCard = styled(Card)`
-    background-color: ${(props) => props.theme.colors.bg.primary};
+    background-color: ${theme.colors.bg.secondary};
 `
 
 const RestaurantCardCover = styled(Card.Cover)`
@@ -13,14 +16,22 @@ const RestaurantCardCover = styled(Card.Cover)`
 `
 
 const Title = styled.Text`
-    font-family:${(props) => props.theme.fonts.body}
+    font-family:${(props) => props.theme.fonts.heading};
+    font-size:${(props) => props.theme.fontSizes.body}
+    color:${(props) => props.theme.colors.ui.primary};
+`
+const Info = styled.View`
     padding:${(props) => props.theme.space[3]};
+`
+const Address = styled(Text)`
+    font-family:${(props) => props.theme.fonts.body};
+    font-size:${(props) => props.theme.fontSizes.caption};
     color:${(props) => props.theme.colors.ui.primary};
 `
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
-        name = 'Algun restaurante chidooooooooo',
+        name = 'Algun restaurante chido',
         icon,
         photos = ['https://www.eluniverso.com/resizer/-miZFOybIZWsLGKD52kMNkTw1BA=/893x670/smart/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/2XYTBX5LUFCRZC3NVYRLQJPBZU.jpg'],
         address = 'alguna calle aleatoria',
@@ -32,7 +43,11 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <>
             <RestaurantCard elevation={10}>
                 <RestaurantCardCover source={{ uri: photos[0] }} />
-                <Title>Hola</Title>
+                <Info>
+                    <Title>{name}</Title>
+                    <SvgXml xml={start} />
+                    <Address>{address}</Address>
+                </Info>
             </RestaurantCard>
         </>
     )
